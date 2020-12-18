3e23dd97a4c75b9c8625efff5427160b695641d2
Victor Lucas <victorlucas.ao@gmail.com>
2020-12-18 14:01:23 -0400

    Parei na aula 40

--- communication-app/src/app/app.module.ts
+++ communication-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,16 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputBindingComponent } from './input-binding/input-binding.component';
 import { ClientComponent } from './input-binding/client/client.component';
+import { EventComponent } from './event/event.component';
+import { ChildItemComponent } from './event/child-item/child-item.component';
+import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputBindingComponent,
-    ClientComponent
+    ClientComponent,
+    EventComponent,
+    ChildItemComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ communication-app/src/app/event/event.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.css']
+})
+export class EventComponent implements OnInit {
+
+  value:number = 0;
+  title:string = 'App Event'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  incBy(event){
+    this.value += event;
+    // console.log(event)
+  }
+}
