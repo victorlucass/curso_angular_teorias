59265a289bdaf46773c5ddc55a679cb4ef1bb2a7
Victor Lucas <victorlucas.ao@gmail.com>
2021-08-03 13:57:20 -0400

    rxjs/from,of,interval

--- operadores-rxjs/src/app/basic-creation/basic-creation.component.ts
+++ operadores-rxjs/src/app/basic-creation/basic-creation.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {from, interval, Observable, Observer, of} from 'rxjs';
 
 @Component({
   selector: 'app-basic-creation',
@@ -7,9 +7,11 @@ import { Observable, Observer } from 'rxjs';
   styleUrls: ['./basic-creation.component.scss'],
 })
 export class BasicCreationComponent implements OnInit {
-  constructor() {}
+  constructor() {
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   observableCreate() {
     const obs = new Observable((observer: Observer<string>) => {
@@ -23,4 +25,29 @@ export class BasicCreationComponent implements OnInit {
       console.log(resource);
     });
   }
+
+  fromClick() {
+    //Modo 1
+    from([1, 2, 3, 4, {x: 10, y: 15}]).subscribe((v) => {
+      console.log(v)
+    });
+    //Modo 2
+    const myFrom = from(['Olá', 'Mundo', 'angular!']);
+    myFrom.subscribe((v) => console.error(v));
+    myFrom.subscribe((v) => console.warn(v));
+  }
+
+  ofClick() {
+    of([1, 2, 3, 4, 5, {x: 10, y: 20}]).subscribe((v) => console.log(v))
+  }
+
+  intervalClick() {
+    const numbers = interval(1000);
+    numbers.subscribe(
+      (x: number) => {
+        console.log("Next " + x);
+      }
+    )
+  }
+
 }
